2499307537f81a7f99a5874ea526225708f531f7
Adam Lathers <alathers@guihack.com,alathers@gmail.com>
2014-03-29 15:20:57 +0100

    bash

--- 2/2.sh
+++ 2/2.sh
@@ -4,3 +4,17 @@
 # alathers@gmail.com
 #
 
+bound=4000000
+fibprev=1
+fibcur=1
+sum=0
+while [ $fibcur -lt $bound ]; do
+	if [ $(($fibcur % 2 )) -eq 0 ]; then 
+		sum=$(($sum+$fibcur))
+	fi
+	fibnext=$(($fibcur+$fibprev))
+	fibprev=$fibcur
+	fibcur=$fibnext
+done
+
+echo $sum
